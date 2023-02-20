7ad9e72eb418e3cd4717cb55068ab2dd8949caab
dancun616 <dancun616@gmail.com>
2023-02-20 09:34:45 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 20, 'M', 'brown', 'cool', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 70, 'M', 'black', 'happy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 6, 'F', 'black', 'sad', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 15, 'M', 'brown', 'angry', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 13, 'F', 'brown', 'shy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 45, 'M', 'brown', 'cool', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 6, 'F', 'black', 'cool', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 15, 'M', 'brown', 'hotheaded', 0);
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,31 +1,19 @@
 def selects_all_female_bears_return_name_and_age
-  <<-SQL 
-      SELECT bears.name, bears.age
-      FROM bears
-      WHERE sex= "F";
-    SQL
+  "SELECT name, age FROM bears WHERE sex='F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  <<-SQL 
-      SELECT name
-      FROM bears
-      ORDER BY name ASC
-    SQL
+  "SELECT name FROM bears ORDER BY name ASC;"
+end
+
+def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
+  "SELECT name, age FROM bears WHERE alive=1 ORDER BY age ASC;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  <<-SQL
-      SELECT bears.name, bears.age
-      FROM bears
-      ORDER BY age DESC LIMIT 1;
-    SQL
-  end
+  "SELECT name, age FROM bears WHERE age = (SELECT MAX(age) FROM bears);"
+end
 
 def select_youngest_bear_and_returns_name_and_age
-  <<-SQL
-    SELECT bears.name, bears.age
-    FROM bears
-    ORDER BY age ASC LIMIT 1;
-  SQL
+  "SELECT name, age FROM bears WHERE age = (SELECT MIN(age) FROM bears);"
 end
